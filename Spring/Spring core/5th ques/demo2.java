628af8f5b3df8797faf3247fd8d7c2890f9694e3
krishnachennamsetty12 <87115038+krishnachennamsetty12@users.noreply.github.com>
2021-08-02 14:43:35 +0530

    Create demo2.java

---
+++ Spring/Spring core/5th ques/demo2.java
@@ -0,0 +1,13 @@
+package com.Spring1.Aquestion5;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class bean2 {
+
+	public void print() {
+		System.out.println("bean2...");
+		
+	}
+
+}
